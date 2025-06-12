737aacc4b2261e56d77db7d5c9ea82a22715b101
澶渊 <chanyuan.lb@alibaba-inc.com>
2025-06-12 15:33:21 +0800

    feat: 添加 ResultDTO 和 ImageFile 类的初始实现

---
+++ src/main/java/com/aliyun/speedpix/examples/dto/ResultDTO.java
@@ -0,0 +1,9 @@
+package com.aliyun.speedpix.examples.dto;
+
+/**
+  * @description
+  *
+  * @author chanyuan.lb
+  * @create 2025-06-12 11:11
+  */ public class ResultDTO {
+}
---
+++ src/main/java/com/aliyun/speedpix/model/ImageFile.java
@@ -0,0 +1,9 @@
+package com.aliyun.speedpix.model;
+
+/**
+  * @description
+  *
+  * @author chanyuan.lb
+  * @create 2025-06-12 11:06
+  */ public class ImageFile {
+}
